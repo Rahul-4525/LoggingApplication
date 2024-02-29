9d8548000a52d8f1897f9cb073a236e60276008c
Rahul <yourstrulyyrahul@gmail.com>
2024-02-29 22:03:33 +0530

    adding controller layer and logback xml

---
+++ src/main/java/com/example/dd/DataDogApp/controller/DataController.java
@@ -0,0 +1,21 @@
+package com.example.dd.DataDogApp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class DataController {
+   Logger logger = LoggerFactory.getLogger(DataController.class);
+    @GetMapping("/getData")
+    public ResponseEntity<String> getData()
+    {
+        logger.info("inside get data class");
+        logger.info("sending data to the client");
+        return ResponseEntity.ok("Hello world!!");
+    }
+}
